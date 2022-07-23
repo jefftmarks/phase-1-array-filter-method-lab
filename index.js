31fd39acd31834edd6ddb54231595b4e09eee026
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-23 14:36:50 -0400

    commit tree b089aedde193b233ac97ee00765733a9b6812c05

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(list, name) {
+    return list.filter((element) => {
+        return element.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(list, name) {
+    return list.filter((element) => {
+        return element.slice(0, 1) === name.slice(0, 1);
+    })
+}
+
+function matchName(list, name) {
+    return list.filter((element) => {
+        return element.name === name;
+    })
+
+}
